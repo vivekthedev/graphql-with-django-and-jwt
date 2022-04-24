6fe8aa0a63c972ac498be4b4376abfbb3b79da65
Vivek Kumar Singh <vivekhappy786@gmail.com>
2022-04-24 09:29:07 +0530

    final project

---
+++ 2_CRUD Final/blog/models.py
@@ -0,0 +1,13 @@
+# blog/models.py
+
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    content = models.TextField()
+    published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
